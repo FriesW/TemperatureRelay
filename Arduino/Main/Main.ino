af781bc2736476c512257e21885d9a0a45561c9d
FriesW <william.james.fries@gmail.com>
2017-12-28 13:48:28 -0600

    WiFi Connection

--- Arduino/Main/Main.ino
+++ Arduino/Main/Main.ino
@@ -8,10 +8,26 @@
 //Flash Size: 512K (64K SPIFFS)
 //Upload Speed: 115200
 
+#include <ESP8266WiFi.h>
+
+#define ssid ""
+#define pass ""
 
 void setup()
 {
-    
+    Serial.begin(115200);
+    delay(10000);
+    Serial.println();
+    Serial.print("Connecting");
+    WiFi.begin(ssid, pass);
+    while(WiFi.status() != WL_CONNECTED)
+    {
+        Serial.print(".");
+        delay(500);
+    }
+    Serial.println("Connected.");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
 }
 
 void loop()
