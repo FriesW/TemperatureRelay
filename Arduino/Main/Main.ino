e1ab17bde1a1493ac7e12be4bfc31dd8fe23f049
FriesW <william.james.fries@gmail.com>
2017-12-28 13:45:18 -0600

    Initial Project Settings

---
+++ Arduino/Main/Main.ino
@@ -0,0 +1,20 @@
+//Target: generic ESP8266
+//Arduino board manager path: http://arduino.esp8266.com/stable/package_esp8266com_index.json
+//Board: Generic ESP8266 Module
+//Flash Mode: DIO
+//Flash Frequency: 40MHz
+//Upload Using: Serial
+//CPU Frequency: 80 MHz
+//Flash Size: 512K (64K SPIFFS)
+//Upload Speed: 115200
+
+
+void setup()
+{
+    
+}
+
+void loop()
+{
+    
+}
