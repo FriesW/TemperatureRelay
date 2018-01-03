477949c1174aa899fd35d2e84cbbb8feb4862fa8
FriesW <william.james.fries@gmail.com>
2018-01-02 18:41:10 -0600

    Index outline

---
+++ Server/Web/index.php
@@ -0,0 +1,54 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+
+<meta charset="utf-8">
+
+<title>Temp History</title>
+
+</head>
+<body>
+
+<h1>Town House Temperatures</h1>
+
+<h3>Quick Glance</h3>
+<table id='quick'>
+<tr>
+    <th>Item</th><th>Value</th>
+</tr>
+<tr>
+    <th>Last sensor check-in:</th><td>3 minutes ago</td>
+</tr>
+<tr>
+    <th>12 hour low:</th><td>45 F</td>
+</tr>
+<tr>
+    <th>36 hour low:</th><td>55 F</td>
+</tr>
+</table>
+
+
+<h3>History: Day</h3>
+<table>
+<tr>
+    <th>Time</th><th>Temperature (F)</th>
+</tr>
+<tr>
+    <td>1/2/2018 6:39P</td><td>46.5</td>
+</tr>
+</table>
+
+<h3>History: Week</h3>
+
+<h3>History: Month</h3>
+
+
+</body>
+
+</html>
